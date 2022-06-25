e2bb8a1e3d8ec1ff63ba35c5ce169a6f3be6e03f
cjdvnd0206 <cjdvndaudnjf@naver.com>
2022-06-25 12:02:45 +0900

    Initial Commit

---
+++ NetworkLayerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkLayerSample
+//
+//  Created by 윤병진 on 2022/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkLayerSample/NetworkLayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerSampleApp.swift
+//  NetworkLayerSample
+//
+//  Created by 윤병진 on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
